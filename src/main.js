8e6140f587864973bdf1908b8589c38748566b42
Saif Kausar <saif@Saifs-MacBook-Pro.local>
2020-06-25 14:52:15 -0700

    bug fix

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ let config = {
     type: Phaser.CANVAS,
     width: 640,
     height: 480,
+    scene: [Menu,Play],
+    
 };
 //create main game object
 let game = new Phaser.Game(config); 
--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -1,5 +1,3 @@
-const { Phaser } = require("../../lib/phaser");
-
 class Menu extends Phaser.Scene {
     constructor(){
         super("menuScene");
