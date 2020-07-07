502ec493217b0162cfe22cbfc0345f84eac8afc8
Saif Kausar <saif@saifs-mbp.lan>
2020-07-07 12:57:51 -0700

    Menu Temp

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -8,15 +8,17 @@ class Menu extends Phaser.Scene {
         this.load.audio('sfx_select', './assets/blip_select12.wav');
         this.load.audio('sfx_explosion', './assets/explosion38.wav');
         this.load.audio('sfx_rocket', './assets/rocket_shot.wav');
+        this.load.image('tempBoat', './assets/Boat.png');
 }
     create(){
-        //menu display
+        //sets the background color of the game
+        this.cameras.main.setBackgroundColor("#64CCFF");
+        //menu display, chooses font, size, color, etc
         let menuConfig = {
             fontFamily: 'Courier',
             fontSize: '28px',
-            backgroundColor: '#F3B141',
-            color: '#843605',
-            align: 'right',
+            color: '#FFF',
+            align: 'center',
             padding: {
                 top: 5,
                 bottom: 5,
@@ -27,12 +29,9 @@ class Menu extends Phaser.Scene {
         let centerY = game.config.height/2;
         let textSpacer = 64;
 
-        this.add.text(centerX, centerY- textSpacer, 'Untitled Trench Game', menuConfig).setOrigin(0.5);
-        this.add.text(centerX, centerY, 'Use <- -> arrows to move & (F) to Fire', menuConfig).setOrigin(0.5);
-        menuConfig.backgroundColor ='#00FF00';
-        menuConfig.color = '#000';
-        this.add.text(centerX, centerY + textSpacer, 'Press <- for Easy or -> for Hard', menuConfig).setOrigin(0.5);
-
+        this.add.text(centerX, centerY - textSpacer * 2, 'Untitled Trench Game', menuConfig).setOrigin(0.5);
+        this.add.rectangle(0, 300, 640, 180, 0x1E53FF).setOrigin(0,0);
+        this.add.image(centerX, centerY + 15, 'tempBoat');
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
         keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
 
