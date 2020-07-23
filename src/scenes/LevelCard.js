2780ff8de35d31e0010671cd4831b2aa15e66903
Dill <dney@ucsc.edu>
2020-07-23 13:34:56 -0700

    research should be fixed

--- src/scenes/LevelCard.js
+++ src/scenes/LevelCard.js
@@ -14,7 +14,16 @@ class LevelCard extends Phaser.Scene {
         console.log(data);
         
         this.researchTier1 = false;
+        if(this.numResearch >= 6)
+            this.researchTier1 = true;
+
         this.researchTier2 = false;
+        if(this.numResearch >= 6)
+        this.researchTier2 = true;
+        
+        this.researchTier3 = false;
+        if(this.numResearch >= 6)
+            this.researchTier3 = true;
     }
 
     preload() {
@@ -22,7 +31,6 @@ class LevelCard extends Phaser.Scene {
 
     create() {
         var passage;
-        var potentialResearch;
         this.cameras.main.setBackgroundColor("#000000");
         this.add.image(game.config.width/2, game.config.height/2, 'wall');
         this.time.delayedCall(1000, () => {
@@ -37,16 +45,6 @@ class LevelCard extends Phaser.Scene {
             loop:true
         });
         keySpace = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
-
-        if(this.numResearch > 5)
-            console.log("> 5");
-        if(this.nextLevel >= 2)
-            console.log('equals');
-
-        if(this.numResearch > 5)
-        {
-            potentialResearch = this.add.text(40, game.config.height / 4 + 60, "New Reseach Unlocked!\n\t Isopods:  \nGadzooks! Look at the girth of that pill bug! \nIf such a small insect can become so large at this depth,\n I shudder to imagine what will follow.\n I'd better cook up a plan to back slang it out of here if need be.", {fontFamily: 'Courier', fontSize: '12px', align: 'left'});
-        }
         
     }
 
@@ -63,19 +61,22 @@ class LevelCard extends Phaser.Scene {
             this.scene.start(this.nextLevel, {depth: this.currDepth, oxy: this.passOxy, numResearch: this.numResearch})
         }
 
-        if(this.numResearch >= 6 && this.nextLevel == 2)
+        if(this.numResearch >= 6 && !this.researchTier1)
         {
             this.add.text(40, game.config.height / 4 + 60, "New Reseach Unlocked!\n\t Isopods:  \nGadzooks! Look at the girth of that pill bug! \nIf such a small insect can become so large at this depth,\n I shudder to imagine what will follow.\n I'd better cook up a plan to back slang it out of here if need be.", {fontFamily: 'Courier', fontSize: '12px', align: 'left'});
+            this.researchTier1 = true;
         }
 
-        if(this.numResearch >= 15 && this.nextLevel == 3)
+        if(this.numResearch >= 15 && !this.researchTier2)
         {
             this.add.text(40, game.config.height / 4 + 60, "New Reseach Unlocked!\n\t Giant Jellies:  \nWell, this is jammiest bits of jam of a jellyfish.\n This is both silly and terrifying.\n They seem to vary in their size immensely.\n Curious... I wonder if gigantification is a property of this trench...", {fontFamily: 'Courier', fontSize: '12px', align: 'left'});
+            this.researchTier2 = true;
         }
 
-        if(this.numResearch >= 28 && this.nextLevel == 4)
+        if(this.numResearch >= 28 && !this.researchTier3)
         {
             this.add.text(40, game.config.height / 4 + 60, "New Reseach Unlocked!\n\t Spider Crabs:  \nYuck, what sort of putrid display is this?\n A spider of crimson of blood and soil. \nWhat horrors to ye toil? Could you be used as fish oil?\n Or will I be shoved off this mortal coil?", {fontFamily: 'Courier', fontSize: '12px', align: 'left'});
+            this.researchTier3 = true;
         }
     }
 }
